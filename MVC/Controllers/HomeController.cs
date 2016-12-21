a825b1a445424a43dc2c89bb0afe3fce936b72c0
royshow0316 <royshow0316@gmail.com>
2016-12-21 17:14:53 +0800

    init

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+using MVC.Models.Interface;
+using MVC.Models.Repository;
+using MVC.ViewModels;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRepository<Project> projectRepository;
+
+        public HomeController()
+        {
+            this.projectRepository = new GenericRepository<Project>();
+        }
+
+        public ActionResult Index()
+        {
+            var projects = projectRepository.GetAll();
+            var viewModel = new ProjectListViewModel();
+            viewModel.ProjectList = projects;
+
+            return View(viewModel);
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new ProjectViewModel();
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            var viewModel = new ProjectViewModel();
+            var model = projectRepository.GetById(x => x.Id == id);
+            viewModel.Id = id;
+            viewModel.Name = model.Name;
+
+            return View("Create", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(ProjectViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (viewModel.Id == Guid.Empty)
+                {
+                    var model = new Project();
+                    model.Id = Guid.NewGuid();
+                    model.Name = viewModel.Name;
+                    projectRepository.Create(model);
+                }
+                else
+                {
+                    var model = projectRepository.GetById(x => x.Id == viewModel.Id);
+                    model.Name = viewModel.Name;
+                    projectRepository.Update(model);
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View("Create", viewModel);
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            var model = projectRepository.GetById(x => x.Id == id);
+            projectRepository.Delete(model);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/Controllers/StructureController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+using MVC.Models.Interface;
+using MVC.Models.Repository;
+using MVC.ViewModels;
+
+namespace MVC.Controllers
+{
+    public class StructureController : Controller
+    {
+        private readonly IRepository<Structure> structureRepository;
+
+        public StructureController()
+        {
+            this.structureRepository = new GenericRepository<Structure>();
+        }
+
+        // GET: Structure
+        public ActionResult Index()
+        {
+            var structures = structureRepository.GetAll().ToList();
+            var viewModel = new StructureListViewModel();
+            viewModel.StructureList = structures;
+
+            return View(viewModel);
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new StructureViewModel();
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            var viewModel = new StructureViewModel();
+            var model = structureRepository.GetById(x => x.Id == id);
+            viewModel.Id = id;
+            viewModel.Name = model.Name;
+
+            return View("Create", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(StructureViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (viewModel.Id == Guid.Empty)
+                {
+                    var model = new Structure();
+                    model.Id = Guid.NewGuid();
+                    model.Name = viewModel.Name;
+                    structureRepository.Create(model);
+                }
+                else
+                {
+                    var model = structureRepository.GetById(x => x.Id == viewModel.Id);
+                    model.Name = viewModel.Name;
+                    structureRepository.Update(model);
+                }
+                return RedirectToAction("Index");
+            }
+            return View("Create", viewModel);
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            var model = structureRepository.GetById(x => x.Id == id);
+            structureRepository.Delete(model);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
